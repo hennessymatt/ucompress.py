9efa1612763704a35ea0016cb3f2571e6190fca0
Matthew Hennessy <matthew.hennessy@bristol.ac.uk>
2024-05-31 14:41:42 +0100

    updated fibre-reinforced materialto allow for hydration

--- ucompress/models/mechanics/fibre_reinforced.py
+++ ucompress/models/mechanics/fibre_reinforced.py
@@ -19,29 +19,27 @@ class FibreReinforced(Hyperelastic):
         self.alpha_f = sp.Symbol('alpha_f')
         self.E_f = sp.Symbol('E_f')
 
-        # Converting E_m and nu_m into mu_m and l_m
-        G_m = self.E_m / 2 / (1 + self.nu_m)
-        l_m = 2 * G_m * self.nu_m / (1 - 2 * self.nu_m)
+        # Converting E and nu into G and the lame parameter
+        G = self.E_m / 2 / (1 + self.nu_m)
+        lame = 2 * G * self.nu_m / (1 - 2 * self.nu_m)
 
-        # Hyperelastic strain energy of the matrix
-        W_m = G_m / 2 * (self.I_1 - 3 - 2 * sp.log(self.J)) + l_m / 2 * (self.J - 1)**2
-
-        # In-plane invariants
-        I_1_x = self.lam_r**2 + self.lam_t**2
+        # Hyperelastic strain energy
+        W_m = G / 2 * (self.I_1 - 3 - 2 * sp.log(self.J_t)) + lame / 2 * (self.J_t - 1)**2
 
         # Subtract off a small amount from lam_t to ensure lam_r > lam_t, 
         # which prevents singularities when lam_r \simeq lam_t
         lam_t = self.lam_t - 1e-4
+        Lam_t = self.beta_r * lam_t
 
         # Strain energy of the fibres
         W_f = self.E_f / 4 * (
-            I_1_x - 8 * self.lam_r / sp.pi * sp.elliptic_e(1 - lam_t**2 / self.lam_r**2) + 2
+            self.I_1_x - 8 * self.Lam_r / sp.pi * sp.elliptic_e(1 - Lam_t**2 / self.Lam_r**2) + 2
             )
 
         # Total strain energy
         self.W = (1 - self.alpha_f) * W_m + self.alpha_f * W_f
 
-        # Update the conversion dictionary
+        # Update the SymPy -> SciPy conversion dictionary
         self.conversion_dict = {
             'elliptic_e': ellipe,
             'elliptic_k': ellipk
@@ -50,26 +48,3 @@ class FibreReinforced(Hyperelastic):
         # Build the symbolic model
         self.build()
     
-    def eval_stress_derivatives(self, lam_r, lam_t, lam_z):
-        """
-        Overloads the method for evaluating the stress derivatives
-        to zero out certain entries and ensure the outputs have
-        the correct shape.
-        """
-
-        N = len(lam_r)
-
-        return (
-            np.diag(self.S_r_r(lam_r, lam_t, lam_z)),
-            np.diag(self.S_r_t(lam_r, lam_t, lam_z)),
-            np.zeros(N),
-
-            np.diag(self.S_t_r(lam_r, lam_t, lam_z)),
-            np.diag(self.S_t_t(lam_r, lam_t, lam_z)),
-            np.zeros(N),
-
-            np.zeros(N),
-            np.zeros(N),
-            self.S_z_z(lam_r, lam_t, lam_z)
-        )
-
