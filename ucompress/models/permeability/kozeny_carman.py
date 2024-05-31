5ce83a1ed9770d1c84e70a1a2fcbf66ab946272b
Matthew Hennessy <matthew.hennessy@bristol.ac.uk>
2024-05-31 17:22:16 +0100

    hack KC permeability

--- ucompress/models/permeability/kozeny_carman.py
+++ ucompress/models/permeability/kozeny_carman.py
@@ -13,8 +13,12 @@ class KozenyCarman(Permeability):
         """
         super().__init__()
 
+        # Introduce a small number to avoid dividing by zero if the initial
+        # porosity is zero (as for a dry hydrogel)
+        eps = 1e-10
+
         # Eulerian permeability
-        self.k = self.k_0 * (1 - self.phi_0)**2 / self.phi_0**3 * self.phi**3 / (1 - self.phi)**2
+        self.k = self.k_0 * (1 - self.phi_0)**2 / (self.phi_0 + eps)**3 * self.phi**3 / (1 - self.phi)**2
         
         # Build the model
         self.build()
